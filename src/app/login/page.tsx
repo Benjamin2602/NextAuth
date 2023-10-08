7cc3e558c0505766e6df955467a2636bb1c72fce
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-10-08 16:10:39 +0530

    file structuring and connecting to DB

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center text-xl text-yellow-400">
+      Login Page
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+  return (
+    <div className="flex justify-center text-3xl text-red-400">
+      SignUp Page
+    </div>
+  );
+}

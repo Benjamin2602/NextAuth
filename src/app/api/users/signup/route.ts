ee6b904d2dc8c26e478d71b16d02bb9e1e48a40a
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-10-13 05:22:27 +0530

    4️⃣ User verification with email (backend)

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 export async function POST(request: NextRequest) {
   try {
@@ -29,6 +30,11 @@ export async function POST(request: NextRequest) {
     });
     //save this to database
     const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    //send verification email
+
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
 
     return NextResponse.json({
       message: "User created successfully",
